5e315c4a292fc81ca40893e7ee4b7133a48dd624
Alberto Pérez <56350482+cfkr-dev@users.noreply.github.com>
2024-10-15 02:01:42 +0200

    Created request method to get meteorological data between 2 dates.

--- src/main/scala/AemetBigDataSparkStudyApp.scala
+++ src/main/scala/AemetBigDataSparkStudyApp.scala
@@ -1,3 +1,12 @@
+import sttp.model.Uri
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 object AemetBigDataSparkStudyApp extends App {
-  val response = HTTPAemetAPIClient.makeRequest
+  val data = HTTPAemetAPIClient.getAllStationsMeteorologicalDataBetweenDates(
+    ZonedDateTime.parse(Constants.startDate).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'UTC'")),
+    ZonedDateTime.parse(Constants.startDate).plusDays(14).plusHours(23).plusMinutes(59).plusSeconds(59).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'UTC'")))
+
+  println(data)
 }
