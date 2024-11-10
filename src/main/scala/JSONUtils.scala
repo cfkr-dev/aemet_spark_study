72f94466979180373e86181ee8c0e74e5b832b41
Alberto Pérez <56350482+cfkr-dev@users.noreply.github.com>
2024-11-10 06:14:19 +0100

    WIP constant refactor

--- src/main/scala/JSONUtils.scala
+++ src/main/scala/JSONUtils.scala
@@ -1,19 +1,8 @@
-import ujson.{Value, write}
+import ujson.write
 
 import java.io.{BufferedWriter, File, FileWriter}
 
 object JSONUtils {
-
-  @deprecated
-  object Aemet {
-    @deprecated
-    def metadataToNamesToTypes(metadata: ujson.Value): Map[String, String] = {
-      metadata("campos").arr.map {
-        campo => campo("id").str -> campo("tipo_datos").str
-      }.toMap
-    }
-  }
-
 
   def writeJSON(file: File, json: ujson.Value, append: Boolean = false): Either[Exception, String] = {
     try {
@@ -26,28 +15,6 @@ object JSONUtils {
       Right(file.getPath)
     } catch {
       case exception: Exception => Left(exception)
-    }
-  }
-
-  @deprecated
-  def cast(json: ujson.Value, typesToCaster: Map[String, ujson.Value => ujson.Value], namesToType: Map[String, String]): ujson.Value = {
-    json match {
-      case obj: ujson.Obj => ujson.Obj.from(
-        obj.value.map {
-          case (key, value) => key -> cast(typesToCaster.get(namesToType.getOrElse(key.toLowerCase, identity(key))) match {
-            case Some(caster) => caster(value)
-            case None => value
-          }, typesToCaster, namesToType)
-        }
-      )
-
-      case arr: ujson.Arr => ujson.Arr(
-        arr.value.map {
-          element => cast(element, typesToCaster, namesToType)
-        }
-      )
-
-      case other => other
     }
   }
 
