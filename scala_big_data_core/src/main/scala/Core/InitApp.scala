0f78e2402623cceffc728ff17953ae36210dc09d
Alberto Pérez <56350482+cfkr-dev@users.noreply.github.com>
2025-11-20 18:33:34 +0100

    Data extraction constants fix

--- scala_big_data_core/src/main/scala/Core/InitApp.scala
+++ scala_big_data_core/src/main/scala/Core/InitApp.scala
@@ -1,15 +1,17 @@
 package Core
 
+import Core.DataExtraction.Aemet.AemetAPIClient
+import Core.DataExtraction.Ifapa.{IfapaAPIClient, IfapaToAemetConverter}
 import Core.PlotGeneration.PlotGenerator
 import Core.Spark.SparkManager.SparkQueries
 
 object InitApp extends App {
 
-  //AemetAPIClient.aemetDataExtraction()
-  //IfapaAPIClient.ifapaDataExtraction()
-  //IfapaToAemetConverter.ifapaToAemetConversion()
-  SparkQueries.execute()
-  PlotGenerator.generate()
+  AemetAPIClient.aemetDataExtraction()
+  IfapaAPIClient.ifapaDataExtraction()
+  IfapaToAemetConverter.ifapaToAemetConversion()
+  //SparkQueries.execute()
+  //PlotGenerator.generate()
 
 
 
