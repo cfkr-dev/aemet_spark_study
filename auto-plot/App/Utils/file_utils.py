07cb3e2300649123d8750e5ff9857f5e055238f7
Alberto Pérez <56350482+cfkr-dev@users.noreply.github.com>
2025-12-10 05:59:38 +0100

    New routes file_utils refactor

--- auto-plot/App/Utils/file_utils.py
+++ auto-plot/App/Utils/file_utils.py
@@ -5,10 +5,16 @@ from App.Config.constants import STORAGE_BASE, OUTPUT_BASE, K_OUTPUT_DIR_NAME
 
 
 def get_src_path(src_path: str):
-    return STORAGE_BASE / Path(src_path)
+    path = Path(src_path)
+    if path.is_absolute():
+        path = path.relative_to("/")
+    return STORAGE_BASE / path
 
 def get_dest_path(dest_path: str):
-    return OUTPUT_BASE / Path(dest_path)
+    path = Path(dest_path)
+    if path.is_absolute():
+        path = path.relative_to("/")
+    return OUTPUT_BASE / path
 
 def get_response_dest_path(dest_path: pathlib.Path):
     try:
